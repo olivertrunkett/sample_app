b0b87d79f10bdd7aea121a58d95b4c4e6b7e19ad
Oliver Trunkett <olivertrunkett@gmail.com>
2012-01-17 16:51:02 -0600

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
-  
   
   def new
     @user = User.new
@@ -54,10 +54,6 @@ class UsersController < ApplicationController
   end
 
   private
-  
-    def authenticate
-      deny_access unless signed_in?
-    end
     
     def correct_user
       @user = User.find(params[:id])
